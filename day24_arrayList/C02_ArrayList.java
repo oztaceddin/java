d9f91929d2dc38bff38c5b0963454e4ea07c9313
taceddin öz <taceddin95@gmail.com>
2021-11-24 21:18:42 +0300

    sonradan eklenenler

---
+++ day24_arrayList/C02_ArrayList.java
@@ -0,0 +1,10 @@
+package day24_arrayList;
+
+public class C02_ArrayList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ day24_arrayList/C04_Remove.java
@@ -0,0 +1,10 @@
+package day24_arrayList;
+
+public class C04_Remove {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
